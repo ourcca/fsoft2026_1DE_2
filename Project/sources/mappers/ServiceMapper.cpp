d7bd61f15677a241e2b9442573f8849cc3ab3bb0
Rodrigo1241122 <1241122@isep.ipp.pt>
2026-06-14 18:08:57 +0100

    Fixing the way time is listed  (ex.: 12:0 --> 12:00)

--- Project/sources/mappers/ServiceMapper.cpp
+++ Project/sources/mappers/ServiceMapper.cpp
@@ -4,9 +4,27 @@ ServiceMapper.cpp
 Created on: 16/05/2026
 */
 #include <string>
+#include <sstream>
+#include <iomanip>
 #include "mappers/ServiceMapper.h"
 #include "model/Date.h"
 #include "model/Time.h"
+
+namespace {
+    std::string formatTime(const Time& time) {
+        int hour;
+        int minute;
+
+        time.getTime(hour, minute);
+
+        std::ostringstream oss;
+        oss << std::setw(2) << std::setfill('0') << hour
+            << ":"
+            << std::setw(2) << std::setfill('0') << minute;
+
+        return oss.str();
+    }
+}
 
 ServiceOutDTO ServiceMapper::toDTO(const Service& service) {
     ServiceOutDTO dto{};
@@ -23,8 +41,7 @@ ServiceOutDTO ServiceMapper::toDTO(const Service& service) {
 
     int hour, minute;
     service.getTime().getTime(hour, minute);
-    dto.time = std::to_string(hour) + ":" +
-               std::to_string(minute);
+    dto.time = formatTime(service.getTime());
 
     dto.animalId = service.getAnimal()->getId();
     dto.veterinarianId = service.getVeterinarian()->getId();
